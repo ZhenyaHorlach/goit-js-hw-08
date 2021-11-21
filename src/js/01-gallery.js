2e5fb1b85e7bc2400f513ca31d5714d23587919d
Zhenya Horlach <gorlachjob@ukr.net>
2021-11-21 18:40:28 +0200

    Добавляет файлы к дз 8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox'
+import 'simplelightbox/dist/simple-lightbox.min.css'
+console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const photosMarkup = createColorPhotosMarkup(galleryItems);
+
+function createColorPhotosMarkup(gallery) {
+  return gallery
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link"  href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+};
+
+galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250,
+});
